90221f2697f14898550c950ff411d6f0ff70e512
Julius Yam <julius.yam@certero.com>
2024-12-30 09:41:22 +0000

    Initial implementation of PaxChartJs

---
+++ BlazorChart/Pages/PaxChartJsPage.razor.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components;
+using pax.BlazorChartJs;
+
+namespace BlazorChart.Pages;
+
+public partial class PaxChartJsPage : ComponentBase
+{
+    ChartJsConfig chartJsConfig = null!;
+    ChartComponent? chartComponent;
+    private bool chartReady;
+
+    protected override void OnInitialized()
+    {
+        chartJsConfig = new ChartJsConfig()
+        {
+            Type = ChartType.bar,
+            Data = new ChartJsData()
+            {
+                Labels = ["Jan", "Feb", "Mar"],
+                Datasets = new List<ChartJsDataset>()
+                    {
+                        new BarDataset()
+                        {
+                            Label = "Dataset 1",
+                            Data = [ 1, 2, 3 ]
+                        }
+                    }
+            }
+        };
+        base.OnInitialized();
+    }
+
+    private void ChartEventTriggered(ChartJsEvent chartJsEvent)
+    {
+        if (chartJsEvent is ChartJsInitEvent initEvent)
+        {
+            chartReady = true;
+        }
+    }
+
+    private void Randomize()
+    {
+        if (!chartReady)
+        {
+            return;
+        }
+
+        List<ChartJsDataset> updateDatasets = [];
+
+        foreach (var dataset in chartJsConfig.Data.Datasets)
+        {
+            if (dataset is BarDataset barDataset)
+            {
+                List<object> newData = new();
+                foreach (var data in barDataset.Data)
+                {
+                    newData.Add(Random.Shared.Next(1, 10));
+                }
+                barDataset.Data = newData;
+                updateDatasets.Add(dataset);
+            }
+        }
+        chartJsConfig.UpdateDatasetsSmooth(updateDatasets);
+    }
+}
--- BlazorChart/Program.cs
+++ BlazorChart/Program.cs
@@ -1,11 +1,19 @@
 using BlazorChart;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using pax.BlazorChartJs;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddChartJs(options =>
+{
+    // default
+    options.ChartJsLocation = "https://cdn.jsdelivr.net/npm/chart.js";
+    options.ChartJsPluginDatalabelsLocation = "https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2";
+});
 
 await builder.Build().RunAsync();
