d37462969448cd968e24c6f2ceb12ae152ca3d84
Julius Yam <julius.yam@certero.com>
2024-12-30 13:46:44 +0000

    Initialising Radzen

--- BlazorChart/Program.cs
+++ BlazorChart/Program.cs
@@ -2,6 +2,7 @@ using BlazorChart;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using pax.BlazorChartJs;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,5 +16,7 @@ builder.Services.AddChartJs(options =>
     options.ChartJsLocation = "https://cdn.jsdelivr.net/npm/chart.js";
     options.ChartJsPluginDatalabelsLocation = "https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2";
 });
+
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
