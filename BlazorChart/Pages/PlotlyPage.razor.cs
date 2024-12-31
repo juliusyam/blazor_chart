6739b3dfbb71dbc5738e516a4af26dcc4910bd75
Julius Yam <julius.yam@certero.com>
2024-12-31 09:48:43 +0000

    Initial implementation of Plotly

---
+++ BlazorChart/Pages/PlotlyPage.razor.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Plotly.Blazor;
+using Plotly.Blazor.Traces;
+using Plotly.Blazor.Traces.ScatterLib;
+
+namespace BlazorChart.Pages;
+
+public partial class PlotlyPage : ComponentBase
+{
+    PlotlyChart chart;
+    Config config = new Config();
+    Plotly.Blazor.Layout layout = new();
+    // Using of the interface IList is important for the event callback!
+    IList<ITrace> data = new List<ITrace>
+    {
+        new Scatter
+        {
+            Name = "ScatterTrace",
+            Mode = ModeFlag.Lines | ModeFlag.Markers,
+            X = [1, 2, 3],
+            Y = [1, 2, 3]
+        }
+    };
+}
