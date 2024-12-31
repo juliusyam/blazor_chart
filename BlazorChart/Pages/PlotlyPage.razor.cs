47e0bcb5ea3e57713524d1274eb507e53c8face4
Julius Yam <julius.yam@certero.com>
2024-12-31 09:55:58 +0000

    More data points for scattered chart

--- BlazorChart/Pages/PlotlyPage.razor.cs
+++ BlazorChart/Pages/PlotlyPage.razor.cs
@@ -9,17 +9,23 @@ namespace BlazorChart.Pages;
 public partial class PlotlyPage : ComponentBase
 {
     PlotlyChart chart;
-    Config config = new Config();
-    Plotly.Blazor.Layout layout = new();
-    // Using of the interface IList is important for the event callback!
-    IList<ITrace> data = new List<ITrace>
-    {
+    private Config config = new();
+    private Plotly.Blazor.Layout layout = new();
+
+    IList<ITrace> data = [
         new Scatter
         {
-            Name = "ScatterTrace",
+            Name = "Dataset 1",
             Mode = ModeFlag.Lines | ModeFlag.Markers,
-            X = [1, 2, 3],
-            Y = [1, 2, 3]
+            X = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            Y = [1, 2, 3, 6, 7, 11, 6, 7, 5]
+        },
+        new Scatter
+        {
+            Name = "Dataset 2",
+            Mode = ModeFlag.Lines | ModeFlag.Markers,
+            X = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            Y = [5, 8, 4, 7, 3, 10, 12, 1, 37]
         }
-    };
+        ];
 }
